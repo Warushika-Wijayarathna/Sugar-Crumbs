3e4de600d7f5c5f36989d4709da53450de04dbe7
Warushika-Wijayarathna <warushikawijayarathna@gmail.com>
2024-10-29 06:32:06 +0530

    mvs architecture applied

---
+++ src/controller/sign_in_controller.js
@@ -0,0 +1,43 @@
+import {users} from '../db/database.js';
+
+//check username and password
+function checkUser(username, password) {
+    return users.find(user => user.username === username && user.password === password);
+}
+
+//enter into the dashboard if the username and password are correct
+function login() {
+    const username = $('#username').val();
+    const password = $('#password').val();
+    const user = checkUser(username, password);
+    if (user) {
+        // Make header, aside, and main content visible
+        $('#header').show();
+        $('#aside').show();
+        $('#main-content').show();
+
+        //load the dashboard
+        loadDashboard();
+
+        // Optionally, hide the login page
+        $('.login-page').hide();
+    } else {
+        alert('Invalid username or password');
+    }
+}
+
+//load the dashboard
+function loadDashboard() {
+    const mainContent = $('.main-content');
+    const dashboardSection = $('.dashboard');
+
+    // Clear existing content
+    mainContent.empty();
+    mainContent.append(dashboardSection);
+    dashboardSection.show();
+}
+
+$(document).ready(function() {
+    let signInBtn = $('#signInBtn');
+    signInBtn.on('click', login);
+});
